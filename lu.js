11c3b45e2971ed4bcdaee62fd422086844a49f32
Simon Kusterer <simon@soped.com>
2014-07-31 22:33:38 +0200

    simplified some stuff

--- lu.js
+++ lu.js
@@ -1,5 +1,39 @@
 !function(glob) {
 
+  var lu = function() {
+    var registry = {}, injectable, container;
+
+    injectable = function(deps, fn) {
+      if (typeof deps === 'function') return deps;
+      deps = pick(deps, registry);
+      return function() {
+        return fn.apply(fn, result(deps));
+      }
+    };
+
+    container = function(deps, fn) {
+      return injectable(deps, fn)();
+    };
+
+    container.service = function(name, deps, fn) {
+      registry[name] = memoize(injectable(deps, fn));
+    };
+
+    container.factory = function(name, deps, fn) {
+      registry[name] = injectable(deps, fn);
+    };
+
+    container.value = function(name, val) {
+      registry[name] = identity(val);
+    };
+
+    container.get = function(name) {
+      return registry[name]();
+    };
+
+    return container;
+  };
+
   var memoize = function(fn) {
     var memo, memoized = false;
     return function() {
@@ -33,54 +67,9 @@
     }
   };
 
-  var lu = function() {
-    var registry = {},
-      injectable,
-      container;
-
-    injectable = function(deps, fn) {
-      if (typeof deps === 'function') return deps;
-      deps = pick(deps, registry);
-      return function() {
-        return fn.apply(fn, result(deps));
-      }
-    };
-
-    container = function(deps, fn) {
-      return injectable(deps, fn)();
-    };
-
-    container.service = function(name, deps, fn) {
-      registry[name] = memoize(injectable(deps, fn));
-      return this;
-    };
-
-    container.factory = function(name, deps, fn) {
-      registry[name] = injectable(deps, fn);
-      return this;
-    };
-
-    container.value = function(name, val) {
-      registry[name] = identity(val);
-      return this;
-    };
-
-    container.get = function(name) {
-      return registry[name]();
-    };
-
-    return container;
-  };
-
   // Node.js / browserify
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = lu;
-  }
-  // AMD
-  else if (typeof define !== 'undefined' && define.amd) {
-    define([], function () {
-      return lu;
-    });
   }
   // <script>
   else {
